7d37909ce567fa011b9e74f5cfb3a0fb2b7377c9
Sarantopoulos Konstantinos <sarantopouloskon@gmail.com>
2022-09-14 14:11:39 +0300

    Don't create tag while running npm version

--- scripts/npm_publish.sh
+++ scripts/npm_publish.sh
@@ -2,9 +2,9 @@
 
 LATEST_GIT_TAG=$(git describe --tags --abbrev=0)
 # Git tag is in the form v1.0.0, remove the prepending "v" to get just the version
-LATEST_GIT_TAG="${latestGitTag:1}"
+LATEST_GIT_TAG="${LATEST_GIT_TAG:1}"
 # Update package.json's version field
-npm version $LATEST_GIT_TAG
+npm version $LATEST_GIT_TAG --no-git-tag-version --no-commit-hooks
 if [[ $LATEST_GIT_TAG == *"beta"* ]];
 then
 echo "Publishing beta version $LATEST_GIT_TAG to npm";
