d088753c81a3e2535e277bac4e6f009d366a550f
SarantopoulosKon <33217757+SarantopoulosKon@users.noreply.github.com>
2022-12-13 19:21:37 +0200

    Embed bundle for CDN (#48)
    * Embed bundle for CDN
    
    * Include css styles in the embed bundle for CDN
    
    * Update package.json's version

---
+++ src/embed.ts
@@ -0,0 +1,7 @@
+export * from './version';
+export * from './widget';
+export * from './clamp';
+export * from './debounce';
+export * from './index';
+export * from './poll';
+import '../style/base.css';
